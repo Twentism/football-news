96c040f9cd73301e37119a8b3affb2bbfcfb904f
Twentism <melantongabriel@gmail.com>
2025-09-17 14:12:00 +0700

    minor revision

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "melanton-gabriel-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://melanton-gabriel-footballnews.pbp.cs.ui.ac.id/"
+    "https://melanton-gabriel-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
